499ec8639115531dba6f92fba466c8425c4dc8fc
jpogzs <56124200+jpogzs@users.noreply.github.com>
2024-06-07 04:40:20 +0800

    Final

--- TechQCNotes.py
+++ TechQCNotes.py
@@ -1,7 +1,22 @@
+# TechQCNotes
+# Extract TechQCNotes from EVM files
+
+# Credits:
+# Idea: Lawrence Trinidad
+# Code: James Pogio
+
 import scipy.io as sio
-import pathlib
 import pandas as pd
 import os
+import time
+
+print("***************************************")
+print("TechQCNotes")
+print("Extract Tech or QC Notes from EVM files")
+print("***************************************")
+print("Idea: LT \t Code: Pogi")
+print()
+time.sleep(3)
 
 path = os.getcwd()
 # print(path)
@@ -11,20 +26,33 @@ fnotes = []
 for root, dirs, files in os.walk(os.path.abspath(path)):
     for file in files:
         if file.endswith(".evm"):
+            evm = ""
+            my_struct = {}
+            notes = ""
             evm = (os.path.join('', file))
             print(evm)
             evmpath = os.path.join(root, file)
-            my_struct = sio.loadmat(evmpath)
-            my_struct.keys()
-            data = my_struct["ThisStructure"]
-            comments = my_struct["Comments"]
-            notes = comments[0,0]["TechQCNotes"][0,0]            
-            stringnotes = str(notes)
-            techqcnotes = stringnotes.replace("['","")
-            techqcnotes = techqcnotes.replace("']","")
-            print(techqcnotes)
-            fname.append(evm)
-            fnotes.append(techqcnotes)
+            try:
+                my_struct = sio.loadmat(evmpath)
+                my_struct.keys()
+                data = my_struct["ThisStructure"]
+                comments = my_struct["Comments"]
+                notes = comments[0,0]["TechQCNotes"][0,0] 
+            except:
+                print("Invalid EVM")
+            
+            finally:
+                           
+                stringnotes = str(notes)
+                techqcnotes = stringnotes.replace("['","")
+                techqcnotes = techqcnotes.replace("']","")
+                techqcnotes = techqcnotes.replace("[","")
+                techqcnotes = techqcnotes.replace("]","")
+                techqcnotes = techqcnotes.replace("-","")
+                print(techqcnotes)
+            
+                fname.append(evm)
+                fnotes.append(techqcnotes)
 
      
 # dictionary of lists
@@ -34,3 +62,10 @@ df = pd.DataFrame(dict)
      
 # saving the dataframe
 df.to_csv('TechQCNotes.csv', index=False)
+
+print()
+print("********************************")
+print("Done")
+print("Results saved in TechQCNotes.csv")
+print("********************************")
+time.sleep(3)
