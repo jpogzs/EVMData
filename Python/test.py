ea83867b9055757346c247698df32c33af8453ea
jpogzs <56124200+jpogzs@users.noreply.github.com>
2024-06-06 19:56:34 +0800

    test

---
+++ Python/test.py
@@ -0,0 +1,9 @@
+
+# import OS module
+import os
+# Get the list of all files and directories
+path = "/"
+dir_list = os.listdir(path)
+print("Files and directories in '", path, "' :")
+# prints all files
+print(dir_list)
